efd227e965efd5860ed7755fc978c2c453f70645
pkwright1998 <gowthampradeep1998@gmail.com>
2024-12-05 16:59:19 +0530

    first-code

---
+++ App.js
@@ -0,0 +1,4 @@
+const header = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},
+    [React.createElement("h1",{},"pradeep"),React.createElement("h2",{},"Kumar")]));
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(header)
